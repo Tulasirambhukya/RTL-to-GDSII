86cda9101e6ea81df400545ec1df934d5ff95a1c
BHUKYA TULASI RAM <95307360+Tulasirambhukya@users.noreply.github.com>
2026-05-11 11:35:24 +0530

    mmmc.tcl

--- Innovus_PnR/mmmc.tcl
+++ Innovus_PnR/mmmc.tcl
@@ -1,26 +1,43 @@
 ############################################################
-# MMMC SETUP (Without RC Corner)
+# MMMC SETUP (WITH CAP TABLE) ✅
 ############################################################
 
-# 1️⃣ Library Sets
+# Library Sets
 create_library_set -name slow_lib \
   -timing { /home/install/FOUNDRY/digital/45nm/LIBS/lib/max/slow.lib }
 
 create_library_set -name fast_lib \
   -timing { /home/install/FOUNDRY/digital/45nm/LIBS/lib/min/fast.lib }
 
-# 2️⃣ Delay Corners (Library Only)
+create_rc_corner -name rc_worst \
+  -qx_tech_file /home/install/FOUNDRY/digital/45nm/LIBS/qx/qrcTechFile \
+  -postRoute_cap {1.1 1.1 1.1} \
+  -postRoute_res {1.1 1.1 1.1} \
+  -postRoute_clkcap {1.1 1.1 1.1} \
+  -postRoute_clkres {1.1 1.1 1.1}
+
+create_rc_corner -name rc_best \
+  -qx_tech_file /home/install/FOUNDRY/digital/45nm/LIBS/qx/qrcTechFile \
+  -postRoute_cap {0.9 0.9 0.9} \
+  -postRoute_res {0.9 0.9 0.9} \
+  -postRoute_clkcap {0.9 0.9 0.9} \
+  -postRoute_clkres {0.9 0.9 0.9}
+
+
+# Delay Corners
 create_delay_corner -name slow_delay \
-  -library_set slow_lib
+  -library_set slow_lib \
+  -rc_corner rc_worst
 
 create_delay_corner -name fast_delay \
-  -library_set fast_lib
+  -library_set fast_lib \
+  -rc_corner rc_best
 
-# 3️⃣ Constraint Mode
+# Constraint Mode
 create_constraint_mode -name func \
-  -sdc_files { /home/Mtech2413/simvision/sync_fifo/sync_fifo_synthesis/output/sync_fifo_syn.sdc }
+  -sdc_files { /home/Mtech2413/rtl_to_gds/fifo_synthesis/output/sync_fifo_syn.sdc }
 
-# 4️⃣ Analysis Views
+# Analysis Views
 create_analysis_view -name setup_view \
   -constraint_mode func \
   -delay_corner slow_delay
@@ -29,5 +46,5 @@ create_analysis_view -name hold_view \
   -constraint_mode func \
   -delay_corner fast_delay
 
-# 5️⃣ Apply Views
+# Apply Views
 set_analysis_view -setup setup_view -hold hold_view
